09002e8e2ad9f69aa9516d69f3719f04f5090161
ingdanielbs <danielb2889@misena.edu.co>
2022-10-13 07:05:48 -0500

    api

--- rick-morty/index.js
+++ rick-morty/index.js
@@ -5,7 +5,19 @@ async function personajes() {
     let personajes = await data.json();
     console.log(personajes.results);
     personajes.results.forEach((personaje) => {
-        info.innerHTML += `${personaje.name}`
+        info.innerHTML += `
+        <div class="col">
+    <div class="card h-100">
+      <img src="${personaje.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${personaje.name}</h5>
+        <p class="card-text">Estado: ${personaje.status}</p>
+        <p class="card-text">Especie: ${personaje.species}</p>
+        <p class="card-text">Género: ${personaje.gender}</p>
+      </div>
+    </div>
+  </div>       
+        `
     })
 }
 
